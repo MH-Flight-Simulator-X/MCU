2a37c282cea9045ec89a2463ba1f81e835ce7bd8
eivindnesje <eivines@stud.ntnu.no>
2024-11-05 16:51:36 +0100

    Removed DS_store and GNU ARM

--- GNU ARM v12.2.1 - Debug/gecko_sdk_4.4.5/platform/common/src/sl_assert.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.4.5/platform/common/src/sl_assert.o: \
- /Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2/platform/common/src/sl_assert.c \
- /Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/inc/sl_assert.h
-/Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/inc/sl_assert.h:
--- GNU ARM v12.2.1 - Debug/gecko_sdk_4.4.5/platform/driver/leddrv/src/sl_led.d
+++
@@ -1,8 +0,0 @@
-gecko_sdk_4.4.5/platform/driver/leddrv/src/sl_led.o: \
- /Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2/platform/driver/leddrv/src/sl_led.c \
- /Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2//platform/driver/leddrv/inc/sl_led.h \
- /Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/inc/sl_status.h
-/Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2//platform/driver/leddrv/inc/sl_led.h:
-/Users/eivindnesje/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/inc/sl_status.h:
