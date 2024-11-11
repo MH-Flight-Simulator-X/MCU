215c5f26441b072352e8dc2685fa6c60a6e71f70
Arash Farzaneh Taleghani <arashfataw@gmail.com>
2024-11-11 19:12:30 +0100

    Move printing out of game

--- inc/display.h
+++ inc/display.h
@@ -2,3 +2,4 @@
 
 void display_init(void);
 void display_write_data(uint8_t reg, uint8_t command);
+void display_print_testing(int iteration);
--- scr/game.cpp
+++ scr/game.cpp
@@ -23,8 +23,6 @@
 
 
 int iteration = 0;
-uint8_t chars[6] = {0x46, 0x55, 0x43, 0x4B, 0x10, 0x10};
-
 
 void game_init()
 {
@@ -39,6 +37,8 @@ void game_process_action(uint32_t frame_counter)
   long double * controllerValues;
   adcValues = read_adc();
   controllerValues = voltage_to_controllerValue(adcValues);
+
+
 
   if (controllerValues[0] > 0 )
   {
@@ -58,12 +58,7 @@ void game_process_action(uint32_t frame_counter)
   }
 
     if (frame_counter % 30 == 0) {
-      display_write_data(0x60, chars[6 % iteration]);
-      display_write_data(0x61, chars[6 % iteration + 1]);
-      display_write_data(0x62, chars[6 % iteration + 2]);
-      display_write_data(0x63, chars[6 % iteration + 3]);
-      display_write_data(0x64, chars[6 % iteration + 4]);
-      display_write_data(0x65, chars[6 % iteration + 5]);
+      display_print_testing(iteration);
       iteration = iteration + 1;
     }
 }
