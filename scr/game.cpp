ca56ef44b0ea3899d5a9fb00c8f0fdddc9f48318
eivindnesje <eivines@stud.ntnu.no>
2024-11-11 19:18:15 +0100

    Added plane to init and process

--- scr/game.cpp
+++ scr/game.cpp
@@ -1,5 +1,6 @@
 #include "sl_simple_led.h"
 #include "sl_simple_led_instances.h"
+#include "plane.h"
 #include "game.h"
 #include "display.h"
 #include "adc_reader.h"
@@ -12,7 +13,6 @@
 #define LED_INSTANCE_1 sl_led_led1
 #endif
 
-
 #ifndef LED_INSTANCE_0
 #define LED_INSTANCE_0 sl_led_led0
 #endif
@@ -21,26 +21,26 @@
 #define LED_INSTANCE_1 sl_led_led1
 #endif
 
-
 int iteration = 0;
 uint8_t chars[6] = {0x46, 0x55, 0x43, 0x4B, 0x10, 0x10};
-
+Plane plane;
 
 void game_init()
 {
-    display_init();
-    adc_init();
+  display_init();
+  adc_init();
+  init_plane(&plane);
 }
 
 void game_process_action(uint32_t frame_counter)
 {
 
-  volatile uint32_t * adcValues;
-  long double * controllerValues;
+  volatile uint32_t *adcValues;
+  long double *controllerValues;
   adcValues = read_adc();
   controllerValues = voltage_to_controllerValue(adcValues);
 
-  if (controllerValues[0] > 0 )
+  if (controllerValues[0] > 0)
   {
     sl_led_turn_on(&LED_INSTANCE_0);
   }
@@ -57,13 +57,15 @@ void game_process_action(uint32_t frame_counter)
     sl_led_turn_off(&LED_INSTANCE_1);
   }
 
-    if (frame_counter % 30 == 0) {
-      display_write_data(0x60, chars[6 % iteration]);
-      display_write_data(0x61, chars[6 % iteration + 1]);
-      display_write_data(0x62, chars[6 % iteration + 2]);
-      display_write_data(0x63, chars[6 % iteration + 3]);
-      display_write_data(0x64, chars[6 % iteration + 4]);
-      display_write_data(0x65, chars[6 % iteration + 5]);
-      iteration = iteration + 1;
-    }
+  if (frame_counter % 30 == 0)
+  {
+    display_write_data(0x60, chars[6 % iteration]);
+    display_write_data(0x61, chars[6 % iteration + 1]);
+    display_write_data(0x62, chars[6 % iteration + 2]);
+    display_write_data(0x63, chars[6 % iteration + 3]);
+    display_write_data(0x64, chars[6 % iteration + 4]);
+    display_write_data(0x65, chars[6 % iteration + 5]);
+    iteration = iteration + 1;
+  }
+  update_plane(&plane);
 }
