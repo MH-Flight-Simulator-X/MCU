957afceb72e5cc049dd0d9719da79291d1251e8f
eivindnesje <eivines@stud.ntnu.no>
2024-11-14 14:19:47 +0100

    Added debugger

--- scr/test.cpp
+++ scr/test.cpp
@@ -3,6 +3,7 @@
 #include "sl_simple_led_instances.h"
 #include "transformation.h"
 #include <stdio.h>
+#include "debug.h"
 #include <string.h>
 
 #ifndef BUTTON_INSTANCE_0
@@ -28,15 +29,7 @@ void sl_button_on_change(const sl_button_t *handle)
     if (&BUTTON_INSTANCE_1 == handle)
     {
       sl_led_toggle(&LED_INSTANCE_1);
-      //MvpMatrixEntry entries[2];
-
-      vec3 position1 = {1.0f, 2.0f, 3.0f};
-      //generate_mvp_matrix_entry(position1, 0.5f, 1.0f, 0xA, 0x1, &entries[0]);
-
-      vec3 position2 = {4.0f, 5.0f, 6.0f};
-      //generate_mvp_matrix_entry(position2, 0.7f, 1.5f, 0xB, 0x2, &entries[1]);
-
-      //fpga_frame_send(entries, 2);
+      debug_print("Knapp 1 trykket");
     }
     if (&BUTTON_INSTANCE_0 == handle)
     {
