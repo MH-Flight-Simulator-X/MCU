4d8404180bf0ae0779121dc822816acaf1c9f160
Ian McGregor Aksum <ian.aksum@gmail.com>
2024-11-15 12:41:56 +0100

    Fixed some build config

--- scr/game.cpp
+++ scr/game.cpp
@@ -36,7 +36,7 @@ Sprite **sprites;
 MvpMatrixEntry *matrix_entries;
 mat4 **mvp_matrices;
 // Can be made into one number where each bit represents an object
-uint32_t hits[sprite_count-1];
+//uint32_t hits[sprite_count-1];
 
 void game_init()
 {
